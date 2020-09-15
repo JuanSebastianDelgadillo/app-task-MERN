67752004d01ab7b59777e39d5c53b47994bf1205
Juan <jsebastian.delgadillo@gmail.com>
2020-09-15 09:22:54 -0300

    app-task MEAN

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const port = 3100;
+
+//SETTING
+
+
+
+//MIDDLEWARES
+
+
+
+//ROUTES
+
+
+
+
+//STATIC FILES
+
+
+
+
+//STARTING THE SERVER
+app.listen(port, () => {
+    console.log(`Server Start on port : ${port}`);
+});
